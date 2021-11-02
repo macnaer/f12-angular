493e88ddcf711367fc088fe4bfd0d31253a3e249
k2 <trofimchuk.an@gmail.com>
2021-11-02 19:46:43 +0200

    Base template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './views/main/main.component';
+import { SidebarComponent } from './views/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
